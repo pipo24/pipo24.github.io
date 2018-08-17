8ab80c965c34493cb804e1755a9c80f5d0d7bf7b
Filippo SMM <filippo.sm.matoso@gmail.com>
2018-08-17 22:46:22 +0100

    cleanup code

--- pages/index.vue
+++ pages/index.vue
@@ -5,11 +5,41 @@
         <span class="word1">Filippo</span>
         <span class="word2">SMM</span>
       </h1>
+      <div class="icons">
+        <a 
+          v-for="(link) in links"
+          :href="link.href" 
+          :key="link.class">
+          <img
+            :class="link.class"
+            :src="link.src"
+            :alt="link.alt" 
+            target="_blank">
+        </a>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
+export default {
+  asyncData (context) {
+    return {
+      links: [
+        {
+          class: "github-icon",
+          href: "//github.com/pipo24",
+          src: "/github.png",
+        },
+        {
+          class: "linkedin-icon",
+          href: "//www.linkedin.com/in/filipposottomayor/",
+          src: "/linkedin.png",
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <style lang="scss">
@@ -70,6 +100,20 @@
       bottom: -5px;
       border-bottom-color: #ffffff;
       border-right-color: #ffffff;
+    }
+  }
+}
+.icons{
+  display: flex;
+  justify-content: space-around;
+  .github-icon, .linkedin-icon{
+    width: 35px;
+    height: 35px;
+  }
+  a:hover{
+    .github-icon, .linkedin-icon{
+      transition: 200ms;
+      opacity: 0.5;
     }
   }
 }
